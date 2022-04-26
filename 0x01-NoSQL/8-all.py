dfe263c5efedcdf19ec7f8afd4f5a09e1f9143dd
rmatovu987 <rmatovu987@gmail.com>
2022-04-26 18:50:45 +0300

    Task 8

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """Function that lists all documents in collection"""
+from typing import List
 
 
-def list_All(mongo_collection):
+def list_All(mongo_collection) -> List:
     """return empty list if no documents"""
-    if mongo_collection.count() == 0:
-        return []
-    else:
+    if mongo_collection is not None:
         return mongo_collection.find()
+    return []
