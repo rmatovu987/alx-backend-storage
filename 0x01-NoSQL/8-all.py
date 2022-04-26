fde5e7d03eac31e6823523aec84a407df827c473
rmatovu987 <rmatovu987@gmail.com>
2022-04-26 18:53:41 +0300

    Task 8

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -8,3 +8,14 @@ def list_All(mongo_collection) -> List:
     if mongo_collection is not None:
         return mongo_collection.find()
     return []
+
+
+if __name__ == '__main__':
+    from pymongo import MongoClient
+
+    list_all = __import__('8-all').list_all
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    school_collection = client.my_db.school
+    schools = list_all(school_collection)
+    for school in schools:
+        print("[{}] {}".format(school.get('_id'), school.get('name')))
