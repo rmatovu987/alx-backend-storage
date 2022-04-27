51c2078950628249c55b0726401864ad78977efc
rmatovu987 <rmatovu987@gmail.com>
2022-04-27 20:24:58 +0300

    Task 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """Create a Cache class. In the __init__ method, store an instance of the Redis client as a private variable named
 _redis (using redis.Redis()) and flush the instance using flushdb. """
 import uuid
-from typing import Union
+from typing import Union, Optional, Callable
 
 import redis
 
@@ -19,3 +19,17 @@ class Cache:
         random_key = str(uuid.uuid1())
         self._redis.mset({random_key: data})
         return random_key
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        """Take a key and return the type"""
+        if fn:
+            return fn(self._redis.get(key))
+        return self._redis.get(key)
+
+    def get_str(self, data: str) -> str:
+        """Convert the bytes to str"""
+        return self._redis.get(data).decode('utf-8')
+
+    def get_int(self, data: str) -> int:
+        """Convert bytes to int"""
+        return int(self._redis.get(data))
